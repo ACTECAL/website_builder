8cea3bdcc4694b4f620ea62b8ca66f4372cb05e2
Sunil Joshi <joshisunil2601@gmail.com>
2026-03-31 18:18:19 +0530

    fix base url

--- src/pages/GetStarted.tsx
+++ src/pages/GetStarted.tsx
@@ -203,7 +203,7 @@ export const GetStarted: React.FC = () => {
       };
 
       const [response] = await Promise.all([
-        fetch("https://api-dev.actecal.com/admin/erp/create", {
+        fetch("https://api-admindev.actecal.com/admin/erp/create", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
